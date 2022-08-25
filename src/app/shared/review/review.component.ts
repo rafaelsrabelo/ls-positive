6acd483014a217f0bd7f5c0f9c95adeb3ed7f5ba
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2022-08-25 08:41:12 -0300

    oi

--- src/app/shared/review/review.component.ts
+++ src/app/shared/review/review.component.ts
@@ -10,44 +10,44 @@ export class ReviewComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.arrowClick();
+    // this.arrowClick();
   }
 
-  arrowClick() {
-    const controls = document.querySelectorAll(".control");
+  // arrowClick() {
+  //   const controls = document.querySelectorAll(".control");
 
-    let currentItem = 0;
-    const items = document.querySelectorAll(".item");
-    const maxItems = items.length
+  //   let currentItem = 0;
+  //   const items = document.querySelectorAll(".item");
+  //   const maxItems = items.length
 
-    controls.forEach((control) => {
-      control.addEventListener("click", () => {
-        const isLeft = control.classList.contains("arrow-left");
+  //   controls.forEach((control) => {
+  //     control.addEventListener("click", () => {
+  //       const isLeft = control.classList.contains("arrow-left");
 
-        if (isLeft) {
-          currentItem -= 1;
-        } else {
-          currentItem += 1;
-        }
+  //       if (isLeft) {
+  //         currentItem -= 1;
+  //       } else {
+  //         currentItem += 1;
+  //       }
 
-        if (currentItem >= maxItems) {
-          currentItem = 0;
-        }
+  //       if (currentItem >= maxItems) {
+  //         currentItem = 0;
+  //       }
 
-        if (currentItem < 0) {
-          currentItem = maxItems - 1;
-        }
+  //       if (currentItem < 0) {
+  //         currentItem = maxItems - 1;
+  //       }
 
-        items.forEach(item => item.classList.remove(".current-item"));
+  //       items.forEach(item => item.classList.remove(".current-item"));
 
-        items[currentItem].scrollIntoView({
-          inline: "center",
-          behavior: "smooth"
-        });
+  //       items[currentItem].scrollIntoView({
+  //         inline: "center",
+  //         behavior: "smooth"
+  //       });
 
-        items[currentItem].classList.add("current-item");
-      });
-    });
-  }
+  //       items[currentItem].classList.add("current-item");
+  //     });
+  //   });
+  // }
 
 }
