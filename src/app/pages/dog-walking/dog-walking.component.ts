21cc3df5b27a43a3990e0d8b9b4f3e6d68a2c820
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2022-09-04 00:11:38 -0300

    dog walking service page finished

--- src/app/pages/dog-walking/dog-walking.component.ts
+++ src/app/pages/dog-walking/dog-walking.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DogWalkingComponent implements OnInit {
   banner = [
-    { title: "Dog Walking", subtitle: " Esse é o momento de lazer e gasto energético do pet. Oferecemos a opção de passeios em 30 minutos ou 50 minutos. Com manejo baseado na metodologia positiva, personalizamos o passeio de acordo com as necessidades do pet", check1: "Momento de interação, sociabilização e exploração dos instintos naturais do dog", check2: "Exclusividade para um cliente por vez (considerando a possibilidade de passeios duplos ou triplos da mesma família)", check3: "Feedback para o tutor com a rota percorrida, relatório descritivo e fotinhas fofas do pet" }
+    { title: "Dog Walking", subtitle: " Esse é o momento de lazer e gasto energético do pet. Oferecemos a opção de passeios em 30 minutos ou 50 minutos. Com manejo baseado na metodologia positiva, personalizamos o passeio de acordo com as necessidades do pet.", check1: "Momento de interação, sociabilização e exploração dos instintos naturais do dog.", check2: "Exclusividade para um cliente por vez (considerando a possibilidade de passeios duplos ou triplos da mesma família).", check3: "Feedback para o tutor com a rota percorrida, relatório descritivo e fotinhas fofas do pet." }
   ]
   constructor() { }
 
